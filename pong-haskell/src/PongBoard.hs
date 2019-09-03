97ac43b3f1a7a5b65a49db8aaac2c3194da07b30
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-09-03 14:00:10 -0300

    Create a basic e a initial structure to pong game
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

---
+++ pong-haskell/src/PongBoard.hs
@@ -0,0 +1,26 @@
+module PongBoard where
+
+import Graphics.Gloss
+
+background :: Color
+background = black
+
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
+data PongGame = Game 
+    { ballLoc :: (Float, Float)
+    , ballVel :: (Float, Float)
+    , player1 :: Float
+    , player2 :: Float
+    }  deriving Show
+
+initialState :: PongGame
+initialState = Game 
+    { ballLoc = (-10, 30)
+    , ballVel = (40, -100)
+    , player1 = 40
+    , player2 = -80
+    }
