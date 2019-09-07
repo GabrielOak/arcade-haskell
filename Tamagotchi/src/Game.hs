ffdeea5825869a41a02130eb1657eb22ab27a874
Guilherme-Aguiar <gui9627oli@gmail.com>
2019-09-07 00:16:01 -0300

    Created input handling and updating functions

--- Tamagotchi/src/Game.hs
+++ Tamagotchi/src/Game.hs
@@ -10,29 +10,33 @@ estadoInicial :: Estados
 estadoInicial = EstadoNormal
     {
         idade = 0,
-        saude = 100,
-        energia = 100,
-        limpeza = 100,
+        saude = 50,
+        energia = 60,
+        limpeza = 50,
         peso = 3,
-        felicidade = 100 
+        felicidade = 50 
     }
 
 fps :: Int
 fps = 60
+
 estadoAtual = brincar estadoInicial
 estadoAtual2 = darRemedio estadoAtual
 
-stringEstado =  renderText 0 0 black $ mostrarStatusUnico 'f' estadoAtual
-
-stringEstado2 =  renderText 0 (-100) blue $ mostrarStatusUnico 'f' estadoAtual2
-
-todos = mostrarStatusTodos estadoAtual2
+todos = mostrarStatusTodos estadoInicial
 
 estados = pictures todos
 
 desenhaNaTela :: Estados -> Picture
-desenhaNaTela game = pictures todos
-
+desenhaNaTela game = pictures $ mostrarStatusTodos game
     
 input :: Event -> Estados -> Estados
+input (EventKey (Char 'c') Down _ _) game = comer game
+input (EventKey (Char 'b') Down _ _) game = brincar game
+input (EventKey (Char 't') Down _ _) game = tomarBanho game
+input (EventKey (Char 'd') Down _ _) game = darRemedio game
+input (EventKey (Char 'e') Down _ _) game = envelhecer game
+input _ game = game
 
+atualizaJogo :: Float -> Estados -> Estados
+atualizaJogo segundos game = game 
