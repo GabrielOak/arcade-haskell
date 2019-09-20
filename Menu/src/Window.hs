0e8a0ef9c62f5ffb95412194eb5a3c826d1091ce
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-09-20 11:06:03 -0300

    Update Menu
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- Menu/src/Window.hs
+++ Menu/src/Window.hs
@@ -20,7 +20,7 @@ module Window where
     
     -- Janela do jogo
     window :: Display
-    window = InWindow "Devilgotchi" (width,height) (offset, offset)
+    window = FullScreen
     
     renderText :: Float -> Float -> Float -> Float-> Color -> String -> Picture
     renderText x y h w cor umaString = translate (x) (y) $ scale h w $ color cor $ text umaString 
